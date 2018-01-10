8935743c41d95949b00bf168af2080d74afa2d4f
Fredrik Soderblom <fredrik@storedsafe.com>
2018-01-10 13:37:19 +0100

    A lookup module for information stored in StoredSafe.

--- README.md
+++ README.md
@@ -1,2 +1,91 @@
-# ansible-storedsafe
-ansible lookup plugin for secrets stored in Password StoredSafe (by StoredSafe)
+# ansible-storedsafe lookup module
+
+This is a lookup module for information stored in [StoredSafe](https://storedsafe.com/). Supports Ansible 2.4 or better.
+
+## Installation
+
+This plugin requires Ansible v2.4 and the Python [```requests```](http://docs.python-requests.org/en/master/) library. 
+
+It has been developed and tested using Python v2.7.12, on Ubuntu 16.04.3 LTS.
+
+Most of the required libraries are installed by default, but requests require manual installation.
+
+**requests:**
+```
+sudo -H pip install requests
+```
+
+Lookup plugins can be loaded from several different locations similar to `$PATH`, see
+[lookup_plugins](http://docs.ansible.com/ansible/intro_configuration.html#lookup-plugins).
+
+The source for the plugin can be pointed to via a _requirements.yml_ file, and accessed via [`ansible-galaxy`](http://docs.ansible.com/ansible/galaxy.html).
+
+## Configuration
+
+Both the StoredSafe server address and the StoredSafe token can be read from the file `$HOME/.storedsafe.rc`, which can be created and maintained by [`storedsafe-tokenhandler`](https://github.com/storedsafe/tokenhandler).
+
+Or it can be done with environment variables or Ansible variables. If any parameter is set by both an environment variable and an alternative means, the environment variable takes precedence.
+
+To specify the address to the StoredSafe server:
+
+    export STOREDSAFE_SERVER=safe.domain.cc
+
+To specify a StoredSafe auth token:
+
+    export STOREDSAFE_TOKEN=<StoredSafe-Token>
+
+The StoredSafe token intentionally can **not** be set via an Ansible variable, as this is generally checked into revision control and would be a bad security practice, defeating the purpose of using StoredSafe. 
+
+
+The plugin also supports specifying the list of trusted CAs either thru by using the ```STOREDSAFE_CABUNDLE``` variables which can either point to a single file or a directory.
+
+	export STOREDSAFE_CABUNDLE=/etc/pki/tls/ca.crt
+	export STOREDSAFE_CABUNDLE=/etc/pki/tls
+
+To disable verification (**NOT RECOMMDED**) the ```STOREDSAFE_SKIP_VERIFY``` variable can be set.
+
+	export STOREDSAFE_SKIP_VERIFY=True
+
+The StoredSafe server address, CA bundle path and validation can also be set via the Ansible variables ```storedsafe_server```, ```storedsafe_cabundle```, and ```storedsafe_skip_verify```, respectively.
+
+## Usage
+
+ansible-storedsafe works as any other lookup plugin.
+
+```yaml
+- debug: msg="{{ lookup('storedsafe', '919/password') }}"
+```
+Where ```919```is the object-id of the requested item. The object-id is visible in the StoredSafe web-ui.
+
+```yaml
+# templates/example.j2
+
+# Generic secrets
+{{ lookup('storedsafe', '919/password') }} # foobar
+# Specify field inside lookup
+{{ lookup('storedsafe', '919/cryptedinfo') }} # foobar
+```
+
+If the desired value is stored within StoredSafe, within a task, the lookup can also be performed with:
+
+```yaml
+with_storedsafe:
+- 919/objectname
+- 919/username
+- 919/password
+```
+
+And then referenced with `"{{ item }}"`
+
+This only work within tasks, though. You can **not** use the `with_storedsafe:` syntax within a variable definition file.
+
+## Thanks
+
+To [Johan Haals](https://github.com/jhaals) for writing the [ansible-vault](https://github.com/jhaals/ansible-vault) lookup module, which has been an inspiration and boilerplate for this plugin.
+
+## Limitations / Known issues
+
+Referencing objects via it's objectname is not yet implemented.
+
+## License
+[Apache-2.0](https://www.apache.org/licenses/LICENSE-2.0)
---
+++ storedsafe.py
@@ -0,0 +1,182 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# (c) 2018, Fredrik Soderblom <fredrik@storedsafe.com>
+# (c) 2018 AB StoredSafe
+
+DOCUMENTATION = """
+    lookup: storedsafe
+    author: Fredrik Soderblom <fredrik@storedsafe.com>
+    version_added: "2.4"
+    short_description: retreive information from vaults in StoredSafe
+    description:
+      - retreive information from vaults in StoredSafe
+    options:
+      <objectid>:
+        description: queried object
+        required: True
+      <fieldname>:
+        description: retrieve value from this field
+        required: True
+"""
+
+EXAMPLES = """
+---
+- hosts: 127.0.0.1
+  tasks:
+    - set_fact: foo_password="{{ lookup('storedsafe', '1337/password') }}"
+    - debug: msg="var is {{ foo_password }} "
+
+    - debug: msg="{{ lookup('storedsafe', '655/pin') }}"
+
+    - name: lookup password in loop
+      debug: msg="{{ item }}"
+      with_storedsafe:
+        - 628/username
+        - 628/password
+        - 628/objectname
+
+# templates/example.j2
+
+# Generic lookup
+{{ lookup('storedsafe', '7893/password') }} # foobar
+
+"""
+
+RETURN = """
+  _list:
+    description: requested value/s
+"""
+
+import os
+import json
+import re
+import requests
+import pprint
+
+from ansible.errors import AnsibleError, AnsibleParserError
+from ansible.plugins.lookup import LookupBase
+from ansible.module_utils._text import to_text
+
+try:
+    from __main__ import display
+except ImportError:
+    from ansible.utils.display import Display
+    display = Display()
+
+class LookupModule(LookupBase):
+
+    def run(self, terms, variables=None, **kwargs):
+
+        result = []
+        display.vvvv(u"StoredSafe lookup initial terms is %s" % (terms))
+
+        cabundle = os.getenv('STOREDSAFE_CABUNDLE') or variables.get('storedsafe_cabundle')
+        skipverify = ((os.getenv('STOREDSAFE_SKIP_VERIFY') in ['1', 'true', 'True', 't']) or
+                       variables.get('storedsafe_skip_verify'))
+
+        server = os.getenv('STOREDSAFE_SERVER') or variables.get('storedsafe_server')
+        token = os.getenv('STOREDSAFE_TOKEN')
+
+        if not server:
+            (server, token) = self._read_rc(os.path.expanduser('~/.storedsafe-client.rc'))
+            if not server:
+                raise AnsibleError('StoredSafe address not set. Specify with'
+                                   ' STOREDSAFE_SERVER environment variable, storedsafe_server Ansible variable'
+                                   ' or specified in the %s' % os.path.expanduser('~/.storedsafe-client.rc'))
+        if not token:
+            raise AnsibleError('StoredSafe token not set. Specify with'
+                               ' STOREDSAFE_TOKEN environment variable'
+                               ' or specify in the %s' % os.path.expanduser('~/.storedsafe-client.rc'))
+            
+        try:
+            url = "https://" + server + "/api/1.0"
+            self._auth_check(url, token, cabundle, skipverify)
+        except:
+            raise AnsibleError('Not logged in to StoredSafe.')
+
+        for term in terms:
+            term_split = term.split('/', 1)
+            objectid = term_split[0]
+            fieldname = term_split[1]
+            display.vvvv(u"StoredSafe lookup using %s/%s" % (objectid, fieldname))
+
+            try:
+                item = self._get_item(url, token, objectid, fieldname, cabundle, skipverify)
+                result.append(item.rstrip())
+            except:
+                raise AnsibleError('Failed to retreive information from StoredSafe.')
+
+        return result
+
+    def _get_item(self, url, token, objectid, fieldname, cabundle, skipverify):
+        item = False
+        payload = { 'token': token, 'decrypt': 'true' }
+        if skipverify:
+            req = requests.get(url + '/object/' + objectid, params=payload, verify=False)
+        elif cabundle:
+            req = requests.get(url + '/object/' + objectid, params=payload, verify=cabundle)
+        else:
+            req = requests.get(url + '/object/' + objectid, params=payload)
+        data = json.loads(req.content)
+        if not req.ok:
+            raise AnsibleError('Failed to communicate with StoredSafe.')
+
+        if (len(data['OBJECT'])): # Unless result is empty
+            try:
+                item = data['OBJECT'][objectid]["crypted"][fieldname]
+            except:
+                try:
+                    item = data['OBJECT'][objectid]["public"][fieldname]
+                except:
+                    try:
+                        item = data['OBJECT'][objectid][fieldname]
+                    except:
+                        item = False
+
+        if not item:
+            raise AnsibleError('Could not find the requested information in StoredSafe.')
+
+        return to_text(item)
+
+    def _read_rc(self, rc_file):
+        if os.path.isfile(rc_file):
+            _file = open(rc_file, 'rU')
+            for line in _file:
+                if "token" in line:
+                    token = re.sub('token:([a-zA-Z0-9]+)\n$', r'\1', line)
+                    if token == 'none':
+                        return (False, False)
+                if "mysite" in line:
+                    server = re.sub('mysite:([a-zA-Z0-9.]+)\n$', r'\1', line)
+                    if server == 'none':
+                        return (False, False)
+            _file.close()
+            if not token:
+                return (False, False)
+            if not server:
+                return (False, False)
+            return (server, token)
+        else:
+            return (False, False)
+
+    def _auth_check(self, url, token, cabundle, skipverify):
+        payload = { 'token': token }
+        try:
+            if skipverify:
+                req = requests.post(url + '/auth/check', data=json.dumps(payload), verify=False)
+            elif cabundle:
+                req = requests.post(url + '/auth/check', data=json.dumps(payload), verify=cabundle)
+            else:
+                req = requests.post(url + '/auth/check', data=json.dumps(payload))
+        except:
+            raise AnsibleError('ERROR: Can not reach "%s"' % url)
+
+        if not req.ok:
+            raise AnsibleError('Not logged in to StoredSafe.')
+
+        data = json.loads(req.content)
+        if data['CALLINFO']['status'] != 'SUCCESS':
+            raise AnsibleError('ERROR: Session not authenticated with server. Token invalid?')
+
+        return True
